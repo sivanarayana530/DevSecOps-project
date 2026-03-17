0e6f9258b5a0cc920cfdc6bd50aadf60fae3fb5f
sivanarayana530 <sivayeturu9@gmail.com>
2026-03-17 22:10:28 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello world! This web application created uisng java spring boot framework and depolyed in networks .";
+		return "Hello world! This web application created uisng java spring boot framework and depolyed in bongu ra   .";
 	}
 }
